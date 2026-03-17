c98db3a0dfbf667e2d7092480cd153f41c34b8e0
k4zunnn <k4zunnn@gmail.com>
2026-03-17 13:01:26 -0300

    Initial commit

---
+++ biblioteca/bibliotecaria/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+class livro(models.Model):
+    nome = models.CharField(max_length=100, unique=True)
+    autor = models.CharField(max_length=100)
+    editora = models.CharField(max_length=100)
+    ano_publicacao = models.IntegerField()
+    genero = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.nome)
+
+class aluno(models.Model):
+    nome_aluno = models.CharField(max_length=100)
+    matricula = models.IntegerField(unique=True)
+    curso = models.CharField(max_length=100)
+    idade = models.IntegerField()
+    def __str__(self):
+       return str(self.nome_aluno)
+
+class avisos(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    data_publicacao = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return str(self.titulo)
+
